39b9720b449ade8bd63a5517eca0d27fe7d19560
Yashas H Majmudar <yashashm.dev@gmail.com>
2025-04-29 19:26:38 -0400

    update: workflow

---
+++ lib/screens/web/wa.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OpenWaScreen extends StatelessWidget {
+  const OpenWaScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
